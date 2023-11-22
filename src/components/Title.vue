0360f251fabbe72ae0b02fc06cae3a206a70052b
AlexanderDerevliov <alex.feelred@gmail.com>
2023-11-22 18:58:14 +0100

    exercise completed and proud of it

---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+    data(){
+        return{
+          title: "Ciao io sono il titolo secondario"
+        }
+    }
+}
+</script>
+
+
+<template>
+<h2>{{ title }}</h2>
+</template>
+
+<style>
+</style>
