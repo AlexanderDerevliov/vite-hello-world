f560ab729cb5a5985f7a6c446abd7dd9a74b9771
AlexanderDerevliov <alex.feelred@gmail.com>
2023-11-23 10:42:32 +0100

    improved with style

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <script>
-import Title from "./components/title.vue";add
+import Title from "./components/title.vue";
 export default {
   components: {
     Title
   },
   data() {
     return {
-      text: "ciao mondo"
+      text: "Hello World"
     }
   }
 }
 </script>
 
 <template>
-<h1>{{ text }}</h1>
-<Title />
+  <h1>{{ text }}</h1>
+  <Title />
 </template>
 
 <style scoped>
+h2 {
+  color: red;
+}
+h1 {
+  color: green;
+}
+h1:hover {
+  color: blue;
+}
 </style>
